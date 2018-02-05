636326b6fb094a789ebfe4aa2f7c89f5405d1500
Lepicsou <donald.voundi@ceduc.cm>
2018-02-05 09:39:21 +0100

    Firt commit de l'exercice

---
+++ GestionStock/Article.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionStock {
+		/*
+	
+	Un article est caractérisé par son numéro de référence, son nom, son prix de vente et une quantité en stock
+	Le stock est représenté par une collection d’articles.
+	  Travail à faire:
+		Créer la classe article contenant les éléments ci-dessous :
+	 */
+	class Article {
+		/*
+		 * Déclaration des attributs,deux ont déjà été déclarés
+		 * Déclarer les deux autres
+		 */
+		// numéro de référence
+		private int reference;
+		// nom
+		private string nom;
+		// prix (ajouter l'attribut)...
+
+		// qte_stock (ajouter la quantité en stock)
+
+		/*
+		 * Déclaration des propriétés
+		 * Une propriété a déjà été déclarée, déclarée les propriétés des autres attributs
+		 */
+
+
+		public int Reference {
+			get { return reference; }
+			set { reference = value; }
+		}
+
+		public string ToString(){
+			// compléter la méthode suisvante
+			return ""; 
+		}
+
+		/// <summary>
+		/// Constructeur par défaut
+		/// Vous pouvez le compléter si possible
+		/// </summary>
+		public Article() {
+			// Compléter au besoin
+		}
+
+		/// <summary>
+		/// Constructeur d'initialisation
+		/// </summary>
+		/// <param name="reference"></param>
+		/// <param name="?"></param>
+		/// <param name="?"></param>
+		/// <param name="?"></param>
+		public Article(int reference, string nom, float prix, int qte_stock){
+			//compléter ici...
+			// Vous pouvez au besoin changer le nom des variables et les types 
+		}
+		
+	}
+}
---
+++ GestionStock/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionStock
+{
+	/*
+Dans la classe Program créer :
+
+Le stock sous forme d'une collection d’articles de votre choix.
+
+Un menu présentant les fonctionnalités suivantes :
+
+1.  Rechercher un article par référence.
+2.  Ajouter un article au stock en vérifiant l’unicité de la référence.
+3.  Supprimer un article par référence.
+4.  Modifier un article par référence.
+5.  Rechercher un article par nom.
+6.  Rechercher un article par intervalle de prix de vente.
+7.  Afficher tous les articles.
+8.  Quitter
+	 */
+	class Program
+	{
+		
+		/// <summary>
+		/// permet de rechercher un article via sa référence
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="reference"></param>
+		/// <returns></returns>
+		static int Search(List<Article> articles, int reference){
+			// A compléter
+			return 0;
+		}
+
+		/// <summary>
+		/// Allow to add an article in the stock if not exist yet
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="article"></param>
+		/// <returns></returns>
+		static bool AddArticle(List<Article> articles, Article article) {
+			// A compléter 
+			return true;
+		}
+
+		/// <summary>
+		/// Allow to delete an article in the stock knowing it reference
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="reference"></param>
+		/// <returns></returns>
+		static bool deleteArticle(List<Article> articles, int reference) {
+			// A compléter
+			return true;
+		}
+
+		/// <summary>
+		/// Allow to modify an article knowing its reference
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="reference"></param>
+		/// <returns></returns>
+		static bool ModifyArticle(List<Article> articles, int reference) {
+			// A compléter....
+			return true;
+		}
+
+		/// <summary>
+		/// Allow to find an article by name
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		static int Search(List<Article> articles, string name) {
+			// A compléter....
+			return 0;
+		}
+
+		/// <summary>
+		/// Allow to to find an article with prize between min and max
+		/// </summary>
+		/// <param name="articles"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+		static Article[] Search(List<Article> articles, int min, int max) {
+			// A compléter....
+			return null;
+		}
+
+		/// <summary>
+		/// Allow to display all the articles in stock 
+		/// </summary>
+		/// <param name="articles"></param>
+		static void Disaplay(List<Article> articles) { 
+			// A compléter....
+		}
+
+		/**/
+		
+		// déclarer les autres propriétés
+		static void Main(string[] args)
+		{
+			int choix, num, quantite, p;
+			double prix;
+			string nom;
+			List<Article> Stock = new List<Article>();
+			System.Console.Out.WriteLine("Compléter le code et effacer cette instruction...");
+			System.Console.In.ReadLine();
+			// A compléter....
+		}
+	}
+}
